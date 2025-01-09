852cd43ff12a734372271e97af2435e32474f2f6
Lolpro <128488714+LolproDoesStuff@users.noreply.github.com>
2025-01-09 17:30:37 +0100

    Update install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,1 +1,9 @@
-echo test
+cd ~
+pkg install git proot wget -y
+git clone https://github.com/MFDGaming/ubuntu-in-termux.git
+cd ubuntu-in-termux
+chmod +x ubuntu.sh
+./ubuntu.sh -y
+echo Done!
+cd ..
+rm install-ubuntu.sh
