ede13574f7e99cbf9eed8472060e8c9946feec70
Lolpro <128488714+LolproDoesStuff@users.noreply.github.com>
2025-01-09 17:43:45 +0100

    Update install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -4,6 +4,8 @@ git clone https://github.com/MFDGaming/ubuntu-in-termux.git
 cd ubuntu-in-termux
 chmod +x ubuntu.sh
 ./ubuntu.sh -y
-echo Done!
 cd ~
 rm install-ubuntu.sh
+clear
+cd ubuntu-in-termux
+echo Done! Run "./startubuntu.sh" to start Ubuntu! 
