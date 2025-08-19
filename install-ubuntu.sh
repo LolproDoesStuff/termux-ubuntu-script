6c4cb821f0ee458df9e89a92cd1725b527e12761
Lolpro <128488714+LolproDoesStuff@users.noreply.github.com>
2025-08-19 11:00:50 +0200

    reverts to ubuntu 22.04 since 24.10 dosen't work
    Thanks to HappyTigerV on this issue
    https://github.com/MFDGaming/ubuntu-in-termux/issues/181

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -2,6 +2,7 @@ cd ~
 pkg install git proot wget -y
 git clone https://github.com/MFDGaming/ubuntu-in-termux.git
 cd ubuntu-in-termux
+sed -i '7s/.*/UBUNTU_VERSION='"'"'22.04'"'"'/' ubuntu.sh
 chmod +x ubuntu.sh
 ./ubuntu.sh -y
 cd ~
